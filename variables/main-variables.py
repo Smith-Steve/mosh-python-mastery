b6672fb74f13ab167eae8f9ab260bc7b3f1471dc
smith-steve <Steve.A.Dore@outlook.com>
2024-03-09 16:06:01 -0800

    variables.

---
+++ variables/main-variables.py
@@ -0,0 +1,9 @@
+students_count = 1000
+rating = 4.99
+is_published = True
+course_name = "Python Programming"
+message = """
+Hi Steve, this is Mosh from Mosh.com
+"""
+
+print(len(course_name[0:4]))
