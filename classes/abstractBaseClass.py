52678988044addf018e503a1218d93e95f10b8eb
smith-steve <Steve.A.Dore@outlook.com>
2024-03-10 20:20:01 -0700

    inheritance work. Class work complete.

--- classes/abstractBaseClass.py
+++ classes/abstractBaseClass.py
@@ -1,8 +1,10 @@
+from abc import ABC, abstractmethod
+
 class InvalidOperationError(Exception):
     #This is a custom exception that inherits from the general class exception
     pass
 
-class Stream:
+class Stream(ABC):
     def __init__(self):
         self.opened = False
     
@@ -15,6 +17,11 @@ class Stream:
         if not self.opened:
             raise InvalidOperationError("Stream is already closed")
         self.opened = False
+    
+    @abstractmethod #This enforces this body of code across all inheritors of this class.
+    def read(self):
+        pass
+
 
 class FileStream(Stream):
     def read(self):
@@ -23,3 +30,13 @@ class FileStream(Stream):
 class NetworkStream(Stream):
     def read(self):
         print("Reading Data from a network")
+
+class MemoryStream(Stream):
+    pass
+#We cannot instantiate a base class.
+# stream = Stream()
+
+
+
+#There is no way to enforce a common interface across all of these streams.
+#An abstract base class has the purpose of providing common code to all objects that inherit for it.
--- classes/multipleInheritance.py
+++ classes/multipleInheritance.py
@@ -10,4 +10,20 @@ class Manager(Employee, Person):
     pass
 
 man = Manager()
+#Python looks at the manager class first. then it looks at the Person class, and then it looks to the employee class.
+#Changing the order of inheritance can change the functionality of an application, because of the overide.
+#
+## Why multi-level inheritance if it can cause problems?
 man.greet()
+
+class Flyer:
+    def fly(self):
+        pass
+
+class Swimmer:
+    def swim(self):
+        pass
+
+class FlyingFish(Flyer, Swimmer):
+    #This is multiple inheritance. It is a good example of it.
+    pass
